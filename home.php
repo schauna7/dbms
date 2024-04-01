8a5383a0362b9729a4f5c9d5710462a38d8934b3
schauna7 <129070130+schauna7@users.noreply.github.com>
2024-04-01 12:08:48 +0530

    Add files via upload

---
+++ home.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title></title>
+    
+    <link rel="stylesheet" type="text/css" href="home.css">
+    <link href="https://fonts.googleapis.com/css?family=Josefin+Sans&display=swap" rel="stylesheet">
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.2/animate.min.css">
+</head>
+<body>
+
+<header>
+    
+<nav>
+    <div class="logo"> <h1 style="font-size: 20px;"> Scholarship Portal </h1> </div>
+    <div class="menu">
+        <a href="#" style="font-size: ">Scholarship</a>
+        <a href="#">Result</a>
+        <a href="" target="_blank">About</a>
+        <a class="btn btn-outline-danger mx-2" type="submit" href="logout.php">Logout</a>
+     
+    </div>
+</nav>
+
+    <main>
+        <section>
+            <h3>Get Scholarship From</h3>
+            <h1>Top Companies Like<span class="change_content"> </span> <span style="margin-top: -10px;"> | </span> </h1>
+            <p>"Grow India Together"</p>
+            <a href="#" class="btnone">learn more</a>
+            
+        </section>
+    </main>
+
+
+</header>
+
+</body>
+</html>
